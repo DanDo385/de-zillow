824d37d616cb616f8cbe61888a49561fb9e7645e
Daniel Magro <dando385@yahoo.com>
2024-05-08 11:15:07 -0400

    restructuring Escrow.test.js

--- test/Escrow.test.js
+++ test/Escrow.test.js
@@ -8,14 +8,38 @@ const tokens = (n) => {
 describe("Escrow", () => {
     let buyer, seller, inspector, lender;
     let realEstate, escrow;
-    
-    it('Saves the addresses', async () => {
-        
+
+    beforeEach(async() => {
+        // Accounts
         [buyer, seller, inspector, lender] = await ethers.getSigners();
-        console.log(buyer, seller);
-        let realEstate = await ethers.getContractFactory('RealEstate')
+        
+        // Deploy RealEstate Contract
+        let RealEstate = await ethers.getContractFactory('RealEstate')
         realEstate = await realEstate.deploy()
 
-       
+        // Mint
+        let transaction = await realEstate.connect(seller).mint("https://ipfs.io/ipfs/QmTudSYeM7mz3PkYEWXWqPjomRPHogcMFSq7XAvsvsgAPS");
+        await transaction.wait();
+
+        // Deploy Escrow Conmtra
+        let Escrow = await ethers.getContractFactory('Escrow');
+        escrow = await escrow.deploy(realEstate.address, seller.address, inspector.address, lender.address);
+
+        // List Property
+        transaction = await escrow.connect(seller).list(1, buyer.address, tokens(10), tokens(5))
+        await transaction.wait()
+        
+        // Property Approval
+        transaction = await RealEstate.connect(seller).approve(escrow.address, 1);
+        await transaction.wait();
     })
-})
+        
+    describe('Deployment', () => {
+        it('Returns NFT address', async () => {
+            let result = await escrow.nftAddress();
+            expect(result).to.be.equal(realEstate.address);
+        })
+    })
+        
+        
+       
